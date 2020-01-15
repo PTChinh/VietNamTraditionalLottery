442535ad6d8ab698398dd550f32dbd6a53217bf2
PTChinh <PTChinh@users.noreply.github.com>
2020-01-15 23:50:12 +0700

    Install dependencies and create first page

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const port = 2020;
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));// for parsing application/x-www-form-urlencoded
+app.use(cookieParser(process.env.SESSION_SECRET));
+
+app.use(express.static('public'));
+
+//Routes
+app.get('/', (req, res) => res.render('main'));
+
+app.listen(port, () => console.log('Server listening on port ' + port));
