2eec23ea0710d6c9281a97bc335dc8263cf5e334
PTChinh <PTChinh@users.noreply.github.com>
2020-01-16 01:18:39 +0700

    Create simple page

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set('views', './views');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));// for parsing application/x-www-form-urlencoded
 app.use(cookieParser(process.env.SESSION_SECRET));
-
+// app.use('/bootstrap', express.static(__dirname + 'node_modules/bootstrap/dist/css'));
 app.use(express.static('public'));
 
 //Routes
